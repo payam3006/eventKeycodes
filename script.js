030691099c21232463b780c47b960739cf9fc2e1
Payam3006 <payam3006@gmail.com>
2024-05-04 03:07:25 +0330

    final

---
+++ script.js
@@ -0,0 +1,19 @@
+const q = console.log;
+let mainPart = 0;
+
+const updateAll = (event) => {
+  if (mainPart == 0) {
+    document.getElementById("landingMessage").classList.add("hidden");
+    document.getElementById("main").classList.remove("hidden");
+
+    mainPart = 1;
+  }
+
+  document.getElementById("eventKey").innerText = `${event.key}`;
+  document.getElementById("eventKeyCode").innerText = `${event.keyCode}`;
+  document.getElementById("eventCode").innerText = `${event.code}`;
+
+  q(event.key);
+  q(event.keyCode);
+  q(event.code);
+};
